50fab25b7f86e979bfd5db04a52ef9048f22bc99
Ungureanu Vladislav <ungureanu.vlaaad@gmail.com>
2024-03-22 01:39:32 +0300

    first commit

---
+++ mystery/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+import Home from './Components/Home/home';
+import Navbar from './Components/Navbar/navbar'; 
+
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
